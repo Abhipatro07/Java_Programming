65312b9b059bc337e24cdcdb4288e1265652be37
Abhishek Patro <102708355+Abhipatro07@users.noreply.github.com>
2024-11-30 16:38:56 +0530

    Initial Commit

---
+++ src/Programming_practice/Power_of_the_number.java
@@ -0,0 +1,22 @@
+package Programming_practice;
+
+import java.util.*;
+
+public class Power_of_the_number {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the base: ");
+		int b = sc.nextInt();
+		System.out.println("Enter the exponent: ");
+		int e = sc.nextInt();
+		int ans = 1;
+		
+		for(int i = 1;i<=e;i++) {
+			ans = ans * b;
+		}
+		
+		System.out.println("The ans is: " + ans);
+	}
+
+}
---
+++ src/Programming_practice/Strong_number_using_funcation.java
@@ -0,0 +1,34 @@
+package Programming_practice;
+
+import java.util.*;
+
+public class Strong_number_using_funcation {
+	public static int factorial(int x) {
+		int fact = 1;
+		for(int i = 1;i<=x;i++) {
+			fact = fact * i;
+		}
+		return fact;
+		
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = sc.nextInt();
+		int temp = n;
+		int sum = 0;
+		
+		while(n != 0) {
+			int rem = n % 10;
+			sum = sum + factorial(rem);
+			n = n / 10;
+		}
+		if(sum == temp) {
+			System.out.println("It's Strong Number");
+		}
+		else {
+			System.out.println("It is not a Strong Number");
+		}
+		
+	}
+}
